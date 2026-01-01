b5d4960c4ef617d229bbeb21aafd05391962db31
Steven Lewi <steven@stevenlewi.id>
2026-01-01 16:08:59 +0700

    Add help message to main executable
    Introduced a printHelp() function that displays usage instructions and argument details for the command-line tool. The help message is now printed when the executable runs.

--- Sources/MyPluginExecutable/main.swift
+++ Sources/MyPluginExecutable/main.swift
@@ -1,3 +1,14 @@
 import Foundation
 
-print("Hello, world!")
+func printHelp() {
+    print("""
+    USAGE: my-plugin-executable -i input_file -o output_file
+    ARGUMENTS:
+      -i input_file           The input file path
+      -o output_file          The output generated file path
+    OPTIONS:
+      -h, --help              Show help information.
+    """)
+}
+
+printHelp()
